910e2a143285c946656e4ba402cf95ff4b571605
Albert <albert.mensahansah@gmail.com>
2020-07-15 17:47:54 +0000

    result UI

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -99,10 +99,15 @@
                       <span class="badge badge-primary">
                           {{ $person->rank }}
                       </span>
-                      <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                      </svg>
-                      <h5 class="mt-0 mb-1">{{ $person->name }}</h5>
+                      
+                    <h5 class="mt-0 mb-1" style="float:left;">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                        </svg>
+                        {{ $person->name }}
+                    </h5>
+                    <span class="badge badge-pill badge-info">{{ $person->Council->council }}</span>
+
                       @if ($person->wasPresent($date)) 
                         <span class="badge badge-pill badge-success">Form Filled</span>
                       @else
