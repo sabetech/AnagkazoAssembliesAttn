bed2d939fc638aacb95157b64c12e64da3c84e83
Albert <albert.mensahansah@gmail.com>
2020-07-15 17:44:22 +0000

    UI mod

--- resources/views/results.blade.php
+++ resources/views/results.blade.php
@@ -94,13 +94,14 @@
                   @foreach($allPersons as $person)
                   
                   <li class="media">
-                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
-                    </svg>
-                    <div class="media-body">
+                    
+                    <div class="media-body shadow-sm p-3 mb-5 bg-white rounded">
                       <span class="badge badge-primary">
                           {{ $person->rank }}
                       </span>
+                      <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-person-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                      </svg>
                       <h5 class="mt-0 mb-1">{{ $person->name }}</h5>
                       @if ($person->wasPresent($date)) 
                         <span class="badge badge-pill badge-success">Form Filled</span>
@@ -108,12 +109,12 @@
                         <span class="badge badge-pill badge-danger">Not Filled</span>
                       @endif
                       <div class="pull-right">
-                        {{ $person->tvOrOnline($date) }}
+                        <span class="badge badge-pill badge-primary">{{ $person->tvOrOnline($date) }}</span>
                       </div>
 
                     </div>
                   </li>
-                  
+
                   @endforeach
 
                 </ul>
