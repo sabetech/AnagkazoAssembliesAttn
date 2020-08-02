e440ce7d34a6cf91712d430720c342c87d34864c
Albert <albert.mensahansah@gmail.com>
2020-08-02 22:56:02 +0000

    just for Ofaakor eiii .. anyways, it's working.. Work for the Lord ..

--- resources/views/council_reports.blade.php
+++ resources/views/council_reports.blade.php
@@ -39,8 +39,73 @@
 
                 <ul class="list-unstyled">
 
+                    @for($i = 10;$i < 16;$i++)
+
+                    <li class="media">
+
+                        <div class="media-body shadow-sm p-3 mb-5 bg-white rounded">
+                            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-building" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd" d="M14.763.075A.5.5 0 0 1 15 .5v15a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5V14h-1v1.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V10a.5.5 0 0 1 .342-.474L6 7.64V4.5a.5.5 0 0 1 .276-.447l8-4a.5.5 0 0 1 .487.022zM6 8.694L1 10.36V15h5V8.694zM7 15h2v-1.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5V15h2V1.309l-7 3.5V15z"/>
+                                <path d="M2 11h1v1H2v-1zm2 0h1v1H4v-1zm-2 2h1v1H2v-1zm2 0h1v1H4v-1zm4-4h1v1H8V9zm2 0h1v1h-1V9zm-2 2h1v1H8v-1zm2 0h1v1h-1v-1zm2-2h1v1h-1V9zm0 2h1v1h-1v-1zM8 7h1v1H8V7zm2 0h1v1h-1V7zm2 0h1v1h-1V7zM8 5h1v1H8V5zm2 0h1v1h-1V5zm2 0h1v1h-1V5zm0-2h1v1h-1V3z"/>
+                              </svg>
+                            <h5 class="mt-0 mb-1">
+                                {{ $councils[$i]->council }} OFAAKOR COUNCIL<br>
+                                <label class="">{{ date("F d, Y", strtotime($date)) }}</label>
+                            </h5>
+                            <label>Number of Pastors Who Prayed
+                                <h4>
+                                    <span class="badge badge-primary">
+                                        {{ $council->getPastorsFlowRatio($date) }}
+                                    </span>
+                                </h4>
+                            </label><br>
+                            <label>Number of Minister shepherds who prayed
+                                <h4>
+                                    <span class="badge badge-secondary">
+                               {{ $council->getMinisterShepheredFlowRatio($date) }}
+
+                                    </span>
+                                </h4>
+                            </label><br>
+                            <label>Number of GWOs who prayed
+                                <h4>
+                                    <span class="badge badge-info">
+                                        {{ $council->getGWO_FlowRatio($date) }}
+                                    </span>
+                                </h4>
+                            </label><br>
+                            <label>Number of Shepherds who prayed
+                                <h4>
+                                    <span class="badge badge-dark">
+                                        {{ $council->getShepherdsWhoFlowed($date) }} /
+                                        {{ $council->getTotalShepherds()->count() }}
+                                    </span>
+                                </h4>
+                            </label><br>
+                            <label>Number of Members who prayed
+                                <h4>
+                                    <span class="badge badge-warning">
+                                        {{ $council->getTotalMembersWhoFlowed($date) }} /
+                                        {{ $council->getTotalMembersAvg() }}
+                                    </span>
+                                </h4>
+                            </label>
+
+                        </div>
+
+                      </li>
+
+                    @endfor
+
+                    <?php $count = 0; ?>
                   @foreach($councils as $council)
-                  <li class="media">
+                    <?php $count++ ?>
+
+                    @if ($count == 5)
+                        @continue;
+                    @endif
+
+                    <li class="media">
 
                     <div class="media-body shadow-sm p-3 mb-5 bg-white rounded">
                         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-building" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
@@ -94,59 +159,6 @@
                     </div>
 
                   </li>
-
-                  @if ($council->id == 5)
-                    @foreach($council->branches as $branch)
-                    <li>
-                        <div class="media-body shadow-sm p-3 mb-5 bg-white rounded">
-                            <h5 class="mt-0 mb-1">
-                            {{ $branch->branch_name }} - {{ $council->council}}<br>
-                                <label class="">{{ date("F d, Y", strtotime($date)) }}</label>
-                            </h5>
-                        <label>Number of Pastors Who Prayed
-                            <h4>
-                                <span class="badge badge-primary">
-                                    {{ $branch->getPastorsFlowRatio($date) }}
-
-                                </span>
-                            </h4>
-                        </label><br>
-                            <label>Number of Minister shepherds who prayed
-                                <h4>
-                                    <span class="badge badge-secondary">
-                            {{ $branch->getMinisterShepheredFlowRatio($date) }}
-
-                                    </span>
-                                </h4>
-                            </label><br>
-                            <label>Number of GWOs who prayed
-                                <h4>
-                                    <span class="badge badge-info">
-                                        {{ $branch->getGWOwhoFlowed($date) }}
-                                    </span>
-                                </h4>
-                            </label><br>
-                            <label>Number of Shepherds who prayed
-                                <h4>
-                                    <span class="badge badge-dark">
-                                        {{ $branch->getShepherdsWhoFlowed($date) }} /
-                                        {{ $branch->getTotalShepherds() }}
-                                    </span>
-                                </h4>
-                            </label><br>
-                            <label>Number of Members who prayed
-                                <h4>
-                                    <span class="badge badge-warning">
-                                        {{ $branch->getTotalMembersWhoFlowed($date) }}
-                                    </span>
-                                </h4>
-                            </label>
-
-                        </div>
-
-                    </li>
-                    @endforeach
-                  @endif
 
                   @endforeach
 
