75b37ff036bb4d385022f0c3720f8905dc17b6a8
Albert <albert.mensahansah@gmail.com>
2020-08-07 19:14:49 +0000

    testing

--- app/Exports/Sheets/ExportCouncilPerSheet.php
+++ app/Exports/Sheets/ExportCouncilPerSheet.php
@@ -43,8 +43,6 @@ class ExportCouncilPerSheet implements FromArray, WithTitle
 
             $exportArray[] = $row;
 
-            dd($person->getShepherdsPresent($this->date));
-
             //save the shepherds ids that were present for a council
             $council_shepherd_ids_that_flowed = array_merge(
                 $council_shepherd_ids_that_flowed,
