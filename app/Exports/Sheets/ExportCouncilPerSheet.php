a5d8befe6a74c98d8b28ba82fed1c68470c044b9
Albert <albert.mensahansah@gmail.com>
2020-08-07 22:38:46 +0000

    test me

--- app/Exports/Sheets/ExportCouncilPerSheet.php
+++ app/Exports/Sheets/ExportCouncilPerSheet.php
@@ -46,10 +46,10 @@ class ExportCouncilPerSheet implements FromArray, WithTitle
             var_dump($person->getShepherdsPresent($this->date));
 
             //save the shepherds ids that were present for a council
-            $council_shepherd_ids_that_flowed = array_merge(
-                $council_shepherd_ids_that_flowed,
-                $person->getShepherdsPresent($this->date)
-            );
+            // $council_shepherd_ids_that_flowed = array_merge(
+            //     $council_shepherd_ids_that_flowed,
+            //     $person->getShepherdsPresent($this->date)
+            // );
         }
 
         $exportArray[] = ['' => ''];
